2f58345bd2e8127d417d90a63ac983e8972162af
lmq150051 <32189206+lmq150051@users.noreply.github.com>
2022-01-12 22:49:46 -0500

    Update deidentify.py

--- cmd/deidentify.py
+++ cmd/deidentify.py
@@ -8,16 +8,19 @@ Options:
     -i FILE
     --replacement char  the PHI replacement char
 """
+import sys
+sys.path.append('../../radtext')
 import bioc
 import docopt
 import tqdm
 
-from cmd.cmd_utils import process_options
+from cmd_utils import process_options
 from radtext.deid import BioCDeidPhilter
 from radtext.pphilter.philter import Philter
 
 if __name__ == '__main__':
     argv = docopt.docopt(__doc__)
+    print('the argv is: ',argv)
     process_options(argv)
 
     philter = Philter()
@@ -30,6 +33,9 @@ if __name__ == '__main__':
         for passage in tqdm.tqdm(doc.passages, leave=False):
             deid.process_passage(passage, doc.id)
             if '--replacement' in argv:
+                print('if is true')
+                if argv['--replacement'] == None:
+                    argv['--replacement'] = 'X'
                 deid.deidentify_passage(passage, argv['--replacement'])
 
     with open(argv['-o'], 'w') as fp:
