0af28cb16bfac03b4e49c7a468aa5c77b484bca7
SONG <wangsong_1996@hotmail.com>
2022-01-13 11:36:37 -0600

    update user guide

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,31 +2,28 @@
 
 export PYTHONPATH=.
 
-source_dir=$HOME'/Subjects/radtext'
-venv_dir=$HOME'/Subjects/venvs/pengyifan-wcm'
-top_dir=$HOME'/Data/ptakes_data'
+top_dir=$HOME'/Results'
 
-disease=pneumoperitoneum
-phrase_file=$top_dir/../${disease}.yml
+# resources files
+ner_phrase_file=$top_dir$/resources/cxr14_phrases_v2.yml
+phrase_file=$top_dir$/resources/chexpert_phrases.yml
 
-prefix=7401
-csv_file=$top_dir/${prefix}.csv
-bioc_file=$top_dir/${}prefix}.xml
-bioc_deid_file=$top_dir/${prefix}_deid.xml
-section_file=$top_dir/${prefix}_section.xml
-ud_file=$top_dir/${prefix}_ud.xml
-ner_file=$top_dir/${prefix}_${disease}.xml
-neg_file=$top_dir/${prefix}_${disease}_neg.xml
-neg_csv_file=$top_dir/${prefix}_${disease}_neg.csv
-
-cd "$source_dir" || exit
-source "${venv_dir}/bin/activate"
+# intermediate output files
+csv_file=$top_dir/$input.csv
+bioc_file=$top_dir/$bioc.xml
+bioc_deid_file=$top_dir/$deid.xml
+section_file=$top_dir/$section.xml
+ud_file=$top_dir/$ud.xml
+ner_file=$top_dir/$ner.xml
+parse_file=$top_dir/$parse.xml
+neg_file=$top_dir/$neg.xml
+neg_csv_file=$top_dir/$output.csv
 
 while [ "$1" != "" ]; do
   case "$1" in
     'csv2bioc' )
       echo "CSV 2 BioC"
-      python cmd/csv2bioc.py -i "$csv_file" -o "$bioc_file" --id_col subject_id --text_col text
+      python cmd/csv2bioc.py -i "$csv_file" -o "$bioc_file"
       ;;
     'deid' )
       echo "de identification"
@@ -34,19 +31,28 @@ while [ "$1" != "" ]; do
       ;;
     'split_section' )
       echo "Split section"
+      pip install medspacy
       python cmd/split_section.py medspacy -i "$bioc_deid_file" -o "$section_file"
       ;;
     'preprocess' )
       echo "Preprocess"
+      pip install -U pip setuptools wheel
+      pip install -U spacy
+      python -m spacy download en_core_web_sm
       python cmd/preprocess_pipeline.py spacy -i "$section_file" -o "$ud_file" --overwrite
       ;;
     'ner' )
       echo "Named entity recognition"
-      python cmd/ner.py regex --phrases "$phrase_file" -i "$ud_file" -o "$ner_file" --overwrite
+      pip install intervaltree
+      python cmd/ner.py regex --phrases "$ner_phrase_file" -i "$ud_file" -o "$ner_file" --overwrite
+      ;;
+    'parse' )
+      echo "Dependency Parsing"
+      python cmd/parse.py -i "$ner_file" -o "$parse_file"
       ;;
     'neg' )
       echo "Negation/Uncertainty detection"
-      python cmd/neg.py -i "$ner_file" -o "$neg_file" --overwrite
+      python cmd/neg.py -i "$parse_file" -o "$neg_file" --overwrite
       ;;
     'collect_neg_label' )
       echo "Collect negation/uncertainty detection"
